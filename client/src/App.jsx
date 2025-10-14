0c56dd4e9deb06ec834996bfa0beeb8ae7cdbb72
Munazza-Iman <imunazza.iman74@gmail.com>
2025-10-14 14:11:14 +0500

    App files uplaod

---
+++ client/src/App.jsx
@@ -0,0 +1,54 @@
+import React, { Profiler } from 'react'
+import { Route, Routes, Navigate } from 'react-router-dom'
+import { useSelector } from "react-redux";
+import SignUp from './Components/SignUp/SignUp.jsx'
+import Login from './Components/Login/Login.jsx'
+import Navbar from './Components/NavBar/Navbar.jsx'
+import Home from './Components/Home/Home.jsx'
+import Explore from './Components/Explore/Explore.jsx'
+import Chat from './Components/Chat/Chat.jsx'
+import Bookmarks from './Components/Bookmarks/Bookmarks.jsx'
+import Profile from './Components/Profile/Profile.jsx'
+import Left from './Components/Left/Left.jsx'
+import Theme from './Components/Theme/Theme.jsx'
+import EditProfile from './Components/EditProfile/EditProfile.jsx'
+
+
+
+function App() {
+  const isAuth = Boolean(useSelector((state) => state.token));
+
+  const OtherPortPage = () => {
+		// You can use window.location.href to redirect to another port
+		window.location.href = "http://localhost:5174";
+		return null; // This is just a placeholder, won't be rendered
+	};
+
+
+  return (
+    <div className="app">
+      <Routes>
+          <Route path="/" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+          <Route exact path="/home" element={isAuth ? <Home /> : <Navigate to="/login" />} />
+          {/* <Route exact path="/chat" element={isAuth ? <Chat /> : <Navigate to="/login" />} /> */}
+          <Route exact path="/profile" element={isAuth ? <Profile /> : <Navigate to="/login" />} />
+          <Route
+							path={"/chat"}
+							element={<OtherPortPage />}
+						/>
+
+          {/* Do we need these routes?? */}
+          <Route exact path="/navbar" element={<Navbar />} />
+          <Route exact path="/explore" element={<Explore />} />
+          <Route exact path="/bookmark" element={<Bookmarks />} />
+          <Route exact path="/left" element={<Left />} />
+          <Route exact path="/theme" element={<Theme />} />
+          <Route exact path="/editprofile" element={<EditProfile />} />
+       </Routes>
+
+    </div>
+  );
+}
+
+export default App;
