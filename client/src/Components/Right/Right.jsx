55656ec10995632b199e512673bea9fcd25c6e40
Munazza-Iman <imunazza.iman74@gmail.com>
2025-10-19 18:31:12 +0500

    Right side structure

---
+++ client/src/Components/Right/Right.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Right.css'
+import FollowersCard from '../FollowersCard/FollowersCard'
+
+function Right() {
+  return (
+    <div className='right'>
+      <FollowersCard />
+    </div>
+  )
+}
+
+export default Right
