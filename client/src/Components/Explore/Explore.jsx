b472f2c83333e7bac625dd88c336edabb8f7965f
Munazza-Iman <imunazza.iman74@gmail.com>
2025-10-25 19:48:02 +0500

    Explore page setup

---
+++ client/src/Components/Explore/Explore.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Navbar from '../NavBar/Navbar'
+import Left from '../Left/Left'
+import './Explore.css'
+import Middle from '../Middle/Middle'
+import Right from '../Right/Right'
+import PostShare from '../PostShare/PostShare'
+import PostCard from '../PostCard/PostCard'
+import Ads from '../Ads/Ads'
+
+function Explore() {
+  return (
+
+    <div className='explore'>
+        <Navbar />
+        <div className="container">
+          <Left />
+          {/* <PostShare /> */}
+          <div className="feeds">
+            {/* PostCard */}
+            <PostCard />
+          </div>
+
+          <Ads />
+          {/* <Right /> */}
+        </div>
+      </div>
+  )
+}
+
+export default Explore
